2e0ecb57358348265ca41b228ddf8888b3365f2e
rootz491 <experimentyt1@gmail.com>
2021-03-21 20:47:26 +0530

    more stuff

--- practice-questions/sumOfDigitUntillSingleDigit.java
+++ practice-questions/sumOfDigitUntillSingleDigit.java
@@ -1,11 +1,37 @@
-import java.lang.*;
+import java.lang.Integer;
 
 class sumOfDigitUntillSingleDigit {
-	public static void main(String[] args) {
-		int sum = 0, i=0;
-		while(i<args.length) {
-			sum += Integer.parseInt(args[i]);
+	public static void main(String[] arg) {
+		int sum = 0;
+		for(int i=0; i<arg.length; i++)
+			sum += Integer.parseInt(arg[i]);
+
+		while(!isSingleDigit(sum)) {
+			int newSum = 0;
+			while(sum!=0) {
+				newSum += sum%10;
+				sum /= 10;
+			}
+			sum = newSum;
 		}
-		System.out.println(sum);
+		System.out.println("sum = "+sum);
+	}
+
+	static boolean isSingleDigit(int num) {
+		if(num/10==0)
+			return true;
+		return false;
 	}
 }
+
+/*
+OUTPUT
+======
+
+
+% javac sumOfDigitUntillSingleDigit.java
+% java sumOfDigitUntillSingleDigit 9 -1 2 3 4
+
+sum = 8
+
+*/
---
+++ practice-questions/sumOfNumOrReverseString.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class sumOfNumOrReverseString {
+	public static void main(String[] args) {
+		Object arr = new Object[];
+		arr[] = {10, "karan", "komal", "nik", 69, 1};
+
+		System.out.println(arr);
+	}
+}
