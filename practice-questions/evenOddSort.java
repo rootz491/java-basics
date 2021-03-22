ea13b9ccc059170f9dfbe0779ff5e5f8a3a71b0a
rootz491 <experimentyt1@gmail.com>
2021-03-23 00:03:10 +0530

    added output

--- practice-questions/evenOddSort.java
+++ practice-questions/evenOddSort.java
@@ -20,7 +20,7 @@ class evenOddSort {
 		for(int i=1; i<s; i++) {
 			int current = i;
 			int j = i-1;
-			while(j>0 && arr[j]%2==0) {
+			while(j=>0 && arr[j]%2==0) {
 				int flag = arr[current];
 				arr[current] = arr[j];
 				arr[j] = flag;
@@ -35,3 +35,40 @@ class evenOddSort {
 		}
 	}
 }
+
+/*
+
+
+% java evenOddSort.java
+enter size of array: 
+8
+enter elements:
+1
+2
+3
+4
+5
+6
+7
+8
+original array: 
+1
+2
+3
+4
+5
+6
+7
+8
+sorted array: 
+1
+3
+5
+7
+8
+6
+4
+2
+
+
+*/
